4a3639b11befd6bdb644f8e7b73d5074230243d7
Katona Adam <sulimiattkene@gmail.com>
2022-05-24 09:48:00 +0200

    validalas

--- src/calc.php
+++ src/calc.php
@@ -2,10 +2,23 @@
 
 echo file_get_contents('templates/head.html');
 $page =  file_get_contents('templates/calc.html');//ezt át kell írni
+
+if(!empty ($_GET['aside'])
+    and !empty ($_GET['bside'])
+    and !empty ($_GET['cside'])
+    and !empty ($_GET['dside'])
+){
+
 $aside = $_GET['aside'];
 $bside = $_GET['bside'];
 $cside = $_GET['cside'];
 $dside = $_GET['dside'];
+$area = calcArea($aside, $bside, $cside, $dside);
+
+}
+else{
+        $area = "Hiba";
+    }
 
 function calcArea($aside, $bside, $cside, $dside){
     $s = ($aside+$bside+$cside+$dside)/2;
