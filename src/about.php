e1b95ce1cbc0f9763bfd3a890893528783386f2f
Katona Adam <sulimiattkene@gmail.com>
2022-05-30 08:40:47 +0200

    fejlec

--- src/about.php
+++ src/about.php
@@ -1,3 +1,13 @@
+<!--
+File: about.php
+Author: Tóth Júlia
+Copyright: 2022, Tóth Júlia
+Group: Szoft 1/n
+Date: 2022-05-30
+Github: https://github.com/haribomaci/
+Licenc: GNU GPL
+-->
+
 <?php
 
 echo file_get_contents('templates/head.html');
