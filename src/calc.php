da176e8aa4c9e53ed1757a2851539393f005f0ab
Katona Adam <sulimiattkene@gmail.com>
2022-05-30 08:36:51 +0200

    szamitas

--- src/calc.php
+++ src/calc.php
@@ -1,33 +1,31 @@
 <?php
 
 echo file_get_contents('templates/head.html');
-$page =  file_get_contents('templates/calc.html');//ezt át kell írni
+$page =  file_get_contents('templates/calc.html');
 
 if(!empty ($_GET['aside'])
     and !empty ($_GET['bside'])
     and !empty ($_GET['cside'])
-    and !empty ($_GET['dside'])
-){
+    ){
 
 $aside = $_GET['aside'];
 $bside = $_GET['bside'];
 $cside = $_GET['cside'];
-$dside = $_GET['dside'];
-$area = calcArea($aside, $bside, $cside, $dside);
+$area = calcArea($aside, $bside, $cside);
 
 }
 else{
         $area = "Hiba";
     }
 
-function calcArea($aside, $bside, $cside, $dside){
-    $s = ($aside+$bside+$cside+$dside)/2;
-    $area = sqrt(($s-$aside)*($s-$bside)*($s-$cside)*($s-$dside));
-    return $area;
+function calcArea($aside, $bside, $cside){
+    $v = (3/4) * M_PI * $aside * $bside * $cside;
+    echo $v;
 }
-$area = calcArea($aside, $bside, $cside, $dside);
 
-$page = str_replace('{{result}}', $area, $page);
-echo $page;
+//$area = calcArea($aside, $bside, $cside);
+
+//$page = str_replace('{{result}}', $v, $page);
+
 
 echo file_get_contents('templates/foot.html');
